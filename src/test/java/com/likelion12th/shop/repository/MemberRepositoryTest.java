6ea1b10bae8ca535b828c7c4205a84cfa860d4bb
yujining3827 <j_yujin_swu22@naver.com>
2025-05-01 15:40:25 +0900

    feat : MemberRepository.java

---
+++ src/test/java/com/likelion12th/shop/repository/MemberRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.likelion12th.shop.repository;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application.properties")
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+}
